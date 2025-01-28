65bf6e2052e82f92de9e41077d8a6832bf7b654b
Fábio Ghizoni <tad-fabioghizoni@ucpparana.edu.br>
2025-01-28 17:32:40 -0300

    👎

--- src/components/Icons/MenuIcon/MenuIcon.tsx
+++ src/components/Icons/MenuIcon/MenuIcon.tsx
@@ -1,6 +1,7 @@
 const MenuIcon = ({ ...props }: React.SVGProps<SVGSVGElement>) => {
   return (
-    <svg {...props} ><rect y="11" width="24" height="2" rx="1" /><rect y="4" width="24" height="2" rx="1" /><rect y="18" width="24" height="2" rx="1" /></svg>
+    <svg {...props} ><rect y="11" width="23" height="2" rx="1" /><rect y="4" width="23" height="2" rx="1" /><rect y="18" width="23" 
+    height="2" rx="1" /></svg>
   );
 };
 
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -17,60 +17,68 @@ const Sidebar = () => {
   };
 
   return (
+    /* ------------- TOPO DA PÁGINA(NAVBAR) ------------- */
     <div className="flex h-[3.5rem] w-full">
       <motion.nav
-        /* ------------- TOPO DA PÁGINA(NAVBAR) ------------- */
-        className="fixed antialiased p-[0.20rem] rounded-br-sm flex flex-col"
-        initial={{ width: "3rem", height: "3.5rem", backgroundColor: `#624D46 ${isOpen ? "#eae0d5" : "#eae0d5"}` }}
+        /* -------------------------- */ 
+        className={`${isOpen ? 'bg-white' : 'bg-transparent' } fixed antialiased p-[0.20rem] rounded-br-md flex-col`}
+        initial={{ 
+          y: -10, 
+        }}
+        whileInView={{
+          y: 0
+        }}
         animate={{
           width: isOpen ? "16rem" : "3rem",
-          height: isOpen ? "20rem" : "3.5rem",
-          backgroundColor: isOpen ? "#ffffff" : "",
+          height: isOpen ? "16rem" : "3.5rem",
         }}
         transition={{
           duration: 0.5,
           ease: "easeInOut",
         }}
       >
-        {/* ------------- BOTÃO DA NAVBAR ------------- */}
         <motion.div
-          transition={{
-            duration: 0.3,
+          key={isOpen ? "open" : "closed"}
+          initial={{ opacity: 0, rotate: 0 }}
+          animate={{
+            opacity: 1,
+            rotate: isOpen ? 90 : 0,
+            transition: { duration: 0.2, ease: "easeInOut" },
+            position: "fixed"
           }}
         >
           {/* ------------- ÍCONE DO BOTÃO DA NAVBAR ------------- */}
-          <div className="border rounded-md w-[50px]">
-          <MenuIcon
-            onClick={toggleMenu}
-            xmlns="http://www.w3.org/2000/svg"
-            id="Outline"
-            viewBox="0 0 26 26"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={1}
-            width={48}
-            height={48}
-            className="rounded-md border pl-[2px] border-black"
-          />
-          </div>
-          <MenuIconX
+          {isOpen ? ( <MenuIconX
             x="0px"
             y="0px"
-            width={18}
-            height={18}
+            width={50}
+            height={48}
             id="Capa_1"
+            fill="#5d4632"
             version="1.1"
+            onClick={toggleMenu}
             xmlSpace="preserve"
             viewBox="0 0 512 512"
             xmlns="http://www.w3.org/2000/svg"
             xmlnsXlink="http://www.w3.org/1999/xlink"
-          />
+            className=" rounded-md py-2"/>
+
+          ) : ( <MenuIcon
+            onClick={toggleMenu}
+            xmlns="http://www.w3.org/2000/svg"
+            id="Outline"
+            viewBox="0 0 23 23"
+            fill="#5d4632"
+            width={50}
+            height={48}
+            className="rounded-md p-1"/>
+          )}
 
         </motion.div>
 
         {/* ------------- LISTA DE ROTAS QUANDO ABRE A NAVBAR ------------- */}
         <motion.div
-          className="flex-grow pt-7"
+          className="flex-grow pt-10"
           initial={{ opacity: 0, x: -20 }}
           animate={{
             opacity: isOpen ? 1 : 0,
@@ -79,7 +87,7 @@ const Sidebar = () => {
           transition={{ duration: 0.5 }}
         >
           <ul className="my-4 pr-1 text-xl">
-            {["Home", "Serviços", "Sobre", "Contatos"].map((item, index) => (
+            {["HOME", "SERVIÇOS", "SOBRE", "CONTATOS"].map((item, index) => (
               <motion.li
                 key={item}
                 className="pl-2 py-1 my-[2px] cursor-pointer rounded-sm transition-all ease-in-out duration-300 
