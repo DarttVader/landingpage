4e3d8d4ddba287ece315e7ffd1244fbe38c7e5f8
Fabio Ghizoni <tad-fabioghizoni@ucpparana.edu.br>
2025-01-31 07:47:10 -0300

    ♈

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,14 @@
 "use client"
-import { IconMenu, IconMenuX } from "@/exports";
+import { IconMenu } from "@/exports";
 import { motion } from "framer-motion";
 import Cinzel from "next/font/local";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 const FontCinzel = Cinzel({
   src: "../../fonts/Cinzel/static/Cinzel-Regular.ttf",
-  variable: "--cinzel-static", 
+  variable: "--cinzel-static",
 });
 
 export default function NavBar() {
@@ -17,17 +18,32 @@ export default function NavBar() {
 
   return (
     <motion.nav className={`${FontCinzel.variable} font-cinzel justify-between flex w-full bg-slate-700`}
-      initial= {{
+      initial={{
 
-      }} 
-      >
-      <Image src="/logonav.png" alt="Logo de Rafaela Mazieiro" width={200} height={200} className="cursor-pointer"/>
+      }}
+    >
+      <Image src="/logo.png" alt="Logo de Rafaela Mazieiro" width={200} height={200} className="cursor-pointer 
+        max-h-[200px] p-1" />
 
       {/* ------------------------------------ MENU HAMBURGER ------------------------------------ */}
 
-      <motion.div onClick={toggleMenu} className= { /* FECHADO */ ` cursor-pointer mt-5 mr-5 rounded-md hover:bg-white/10 w-[50px] h-[50px]`}>
-        <IconMenu className={ /* FECHADO */ ` rounded-md text-[#DE9091] p-1`} width="50" height="50"></IconMenu>
-        <IconMenuX className={ /* FECHADO */ `  border rounded-md text-[#DE9091] p-1`} width="50" height="50"></IconMenuX>
+      <motion.div className={isOpen ?
+        /* ABERTO */ "w-72 mt-0 bg-white h-full " :
+        /* FECHADO */ "cursor-pointer mt-5 mr-5 rounded-md hover:bg-white/10 w-[50px] h-[50px]"}>
+
+        <IconMenu className={isOpen ?
+          /* ABERTO */ "hidden" :
+          /* FECHADO */ "rounded-md text-[#DE9091] p-1"}
+          onClick={toggleMenu} width="50" height="50"></IconMenu>
+
+        <motion.ul className={isOpen ?
+          /* ABERTO */ "" :
+          /* FECHADO */ "hidden"}>
+          <Link href="">Inicio</Link>
+          <Link href=""></Link>
+          <Link href="">Sobre</Link>
+          <Link href="">Contatos</Link>
+        </motion.ul>
       </motion.div>
     </motion.nav>
   );
--- src/exports.ts
+++ src/exports.ts
@@ -3,5 +3,4 @@ export { default as NavBar } from "@/components/NavBar/NavBar";
 export { default as SectionEnd } from "@/components/SectionEnd/SectionEnd";
 export { default as SectionTop } from "@/components/SectionTop/SectionTop";
 export { default as IconMenu } from "../public/IconMenu";
-export { default as IconMenuX } from "../public/IconMenuX";
 
