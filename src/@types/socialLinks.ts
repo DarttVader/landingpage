39869de13e4caf8f5f42c96c3c8fcbb0468394c4
Fabio Ghizoni <tad-fabioghizoni@ucpparana.edu.br>
2025-04-15 00:03:16 -0300

    feat: Alteração de link para whatsapp da clínica e alteração de uma imagem!

--- src/@types/socialLinks.ts
+++ src/@types/socialLinks.ts
@@ -1,7 +1,7 @@
 import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa6";
 
 export const socialLinks = [
-  { Href: "http://wa.me/554391030509", Icon: FaWhatsapp, Title: "Whatsapp Rafaela Mazieiro" },
+  { Href: "http://wa.me/5543996134770", Icon: FaWhatsapp, Title: "Whatsapp Rafaela Mazieiro" },
   { Href: "https://www.instagram.com/drarafamazieiro", Icon: FaInstagram, Title: "Instagram Rafaela Mazieiro" },
   { Href: "https://www.facebook.com/rafaela.mazieiro", Icon: FaFacebook, Title: "Facebook Rafaela Mazieiro" },
 ];
